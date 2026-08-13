3bf723fdbe8ccc457e0ed7b45c622c812388f7e8
Piyush Kohli <piiussh@gmail.com>
2026-08-14 01:44:41 +0530

    added query for searchuser in getUser method

--- backend/src/main/java/com/netflix/clone/dao/UserRepository.java
+++ backend/src/main/java/com/netflix/clone/dao/UserRepository.java
@@ -1,9 +1,13 @@
 package com.netflix.clone.dao;
 
 import com.netflix.clone.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.netflix.clone.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -18,4 +22,10 @@ public interface  UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPasswordResetToken(String passwordResetToken);
 
     long countByRoleAndActive(Role role, boolean active);
+
+    @Query("SELECT u FROM User u WHERE " + "LOWER(u.fullName) LIKE LOWER(CONCAT('%',:search,'%')) OR "
+    + "LOWER(u.email) LIKE LOWER(CONCAT('%',:search,'%'))")
+    Page<User> searchUsers(@Param("search") String search, Pageable pageable);
+
+    long countByRole(Role role);
 }
