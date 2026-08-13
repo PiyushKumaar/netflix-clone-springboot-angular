d97b1b4c58d676dbe1b1aa7506749e0a27df8eb9
Piyush Kohli <piiussh@gmail.com>
2026-08-13 15:22:06 +0530

    added findByEmail,existsByEmail , findByVerificationToken,findByPasswordResetToken

--- backend/src/main/java/com/netflix/clone/dao/UserRepository.java
+++ backend/src/main/java/com/netflix/clone/dao/UserRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.netflix.clone.entity.User;
 
+import java.util.Optional;
+
 public interface  UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByVerificationToken(String verificationToken);
+
+    Optional<User> findByPasswordResetToken(String passwordResetToken);
+
 
 }
