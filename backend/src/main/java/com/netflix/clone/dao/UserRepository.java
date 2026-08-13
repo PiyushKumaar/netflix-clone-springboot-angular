4ccee194ec63ca6a45fe69111efd0e6cc203982b
Piyush Kohli <piiussh@gmail.com>
2026-08-13 23:53:44 +0530

    introduced new method to count the user by role and active

--- backend/src/main/java/com/netflix/clone/dao/UserRepository.java
+++ backend/src/main/java/com/netflix/clone/dao/UserRepository.java
@@ -1,5 +1,6 @@
 package com.netflix.clone.dao;
 
+import com.netflix.clone.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.netflix.clone.entity.User;
@@ -16,5 +17,5 @@ public interface  UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPasswordResetToken(String passwordResetToken);
 
-
+    long countByRoleAndActive(Role role, boolean active);
 }
