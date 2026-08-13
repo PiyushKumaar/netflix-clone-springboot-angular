c74dceea9344615829d1d8d690a7071db8b2f063
Piyush Kohli <piiussh@gmail.com>
2026-08-13 15:38:53 +0530

    currentUser method in the class

--- backend/src/main/java/com/netflix/clone/serviceImpl/AuthServiceImpl.java
+++ backend/src/main/java/com/netflix/clone/serviceImpl/AuthServiceImpl.java
@@ -166,4 +166,10 @@ public class AuthServiceImpl implements AuthService {
 
        return new MessageResponse("Password changed successfully");
     }
+
+    @Override
+    public LoginResponse currentUser(String email) {
+        User user = serviceUtils.getUserByEmailOrThrow(email);
+        return new LoginResponse(null , user.getEmail(), user.getFullName(), user.getRole().name());
+    }
 }
