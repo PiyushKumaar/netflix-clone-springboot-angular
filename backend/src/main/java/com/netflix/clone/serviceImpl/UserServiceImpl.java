238d7825f74657caf82f78521f42dcc7768e0b63
Piyush Kohli <piiussh@gmail.com>
2026-08-13 23:54:20 +0530

    class for user service implementaion

---
+++ backend/src/main/java/com/netflix/clone/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,91 @@
+package com.netflix.clone.serviceImpl;
+
+import com.netflix.clone.dao.UserRepository;
+import com.netflix.clone.dto.request.UserRequest;
+import com.netflix.clone.dto.response.MessageResponse;
+import com.netflix.clone.entity.User;
+import com.netflix.clone.enums.Role;
+import com.netflix.clone.exceptions.EmailAlreadyExistException;
+import com.netflix.clone.exceptions.InvalidRoleException;
+import com.netflix.clone.service.EmailService;
+import com.netflix.clone.service.UserService;
+import com.netflix.clone.util.ServiceUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ServiceUtils serviceUtils;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Override
+    public MessageResponse createUser(UserRequest userRequest) {
+        if(userRepository.findByEmail(userRequest.getEmail()).isPresent()){
+            throw new EmailAlreadyExistException(("Email already exists"));
+        }
+
+        validateRole(userRequest.getRole());
+
+        User user = new User();
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(userRequest.getPassword());
+        user.setFullName(userRequest.getFullName());
+        user.setRole(Role.valueOf(userRequest.getRole().toUpperCase()));
+        user.setActive(true);
+
+        String verificationToken = UUID.randomUUID().toString();
+
+        user.setVerificationToken(verificationToken);
+        user.setVerificationTokenExpiry(Instant.now().plusSeconds(86400));
+
+        userRepository.save(user);
+        emailService.sendVerificationEmail(userRequest.getEmail(), verificationToken);
+
+        return new MessageResponse("User created successfully, verification email has sent to the user email inbox to verify email");
+    }
+
+    private void validateRole(String role) {
+        if (Arrays.stream(Role.values()).noneMatch(r->r.name().equalsIgnoreCase(role))){
+            throw new InvalidRoleException("Invalid role : "+ role);
+        }
+    }
+
+    @Override
+    public MessageResponse updateUser(Long id, UserRequest userRequest) {
+        User user = serviceUtils.getUserByIdOrThrow(id);
+
+        ensureNotLastActiveAdmin(user);
+        validateRole(userRequest.getRole());
+
+        user.setFullName(userRequest.getFullName());
+        user.setRole(Role.valueOf(userRequest.getRole().toUpperCase()));
+        user.setActive(userRequest.getActive());
+        userRepository.save(user);
+
+        return new MessageResponse("User updated successfully");
+    }
+
+    private void ensureNotLastActiveAdmin(User user) {
+        if(user.isActive() && user.getRole() == Role.ADMIN){
+            long activeAdminCount = userRepository.countByRoleAndActive(Role.ADMIN,true);
+            if(activeAdminCount <=1){
+                throw new RuntimeException("Cannot deactivate the last active admin user");
+            }
+        }
+    }
+}
