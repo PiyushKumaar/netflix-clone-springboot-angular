3020973f05624c6fb0464cd4f1e2a9ed49732424
Piyush Kohli <piiussh@gmail.com>
2026-08-13 15:17:25 +0530

    added all args construtor annnotation

--- backend/src/main/java/com/netflix/clone/dto/response/EmailValidationResponse.java
+++ backend/src/main/java/com/netflix/clone/dto/response/EmailValidationResponse.java
@@ -1,10 +1,12 @@
 package com.netflix.clone.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class EmailValidationResponse {
 
     private boolean exists;
