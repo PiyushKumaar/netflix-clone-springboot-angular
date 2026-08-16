7b547c863b7276c74ebc5e522281c5ad384391bb
Piyush Kohli <piiussh@gmail.com>
2026-08-16 19:25:27 +0530

    added routes path for Landing and signup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Landing } from './landing/landing';
+import { Signup } from './signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Landing},
+    {path:'signup',component:Signup},
+    {path:'**',redirectTo:'',pathMatch:'full'}
+    //login
+];
