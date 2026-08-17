5f52b01c372a68ff40f571314928784084e4ba5c
Piyush Kohli <piiussh@gmail.com>
2026-08-17 16:36:29 +0530

    added login and verify-email routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing';
 import { Signup } from './signup/signup';
+import { Login } from './login/login';
+import { VerifyEmail } from './verify-email/verify-email';
 
 export const routes: Routes = [
     {path:'',component:Landing},
     {path:'signup',component:Signup},
+    {path:'login',component:Login},
+    {path:'verify-email',component:VerifyEmail},
     {path:'**',redirectTo:'',pathMatch:'full'}
-    //login
 ];
