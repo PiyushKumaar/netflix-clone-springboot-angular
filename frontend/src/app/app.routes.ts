6214a6f496be37e6e505f0c85b5021ce14a2592c
Piyush Kohli <piiussh@gmail.com>
2026-08-18 11:46:51 +0530

    added routes for home,authguard,adminguard, and forgotpassword

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,22 @@ import { Landing } from './landing/landing';
 import { Signup } from './signup/signup';
 import { Login } from './login/login';
 import { VerifyEmail } from './verify-email/verify-email';
+import { Home } from './user/home/home';
+import { authGuard } from './shared/guards/auth-guard';
+import { adminGuard } from './shared/guards/admin-guard';
+import { ForgotPassword } from './forgot-password/forgot-password';
 
 export const routes: Routes = [
     {path:'',component:Landing},
     {path:'signup',component:Signup},
     {path:'login',component:Login},
     {path:'verify-email',component:VerifyEmail},
+    {path:'forgot-password',component:ForgotPassword},
+    {path:'home',component:Home, canActivate:[authGuard]},
+    {
+        path:'admin',
+        loadChildren:()=>import('./admin/admin-module').then(m=>m.AdminModule),
+        canActivate:[adminGuard]
+    },
     {path:'**',redirectTo:'',pathMatch:'full'}
 ];
