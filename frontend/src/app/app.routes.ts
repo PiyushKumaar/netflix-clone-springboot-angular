39b1f8504df3537fb7999c2cb62fb9d09d9b339d
Piyush Kohli <piiussh@gmail.com>
2026-08-18 13:07:33 +0530

    added route for reset-password

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { Landing } from './landing/landing';
-import { Signup } from './signup/signup';
-import { Login } from './login/login';
+import { Landing } from './components/landing/landing';
+import { Signup } from './components/signup/signup';
+import { Login } from './components/login/login';
 import { VerifyEmail } from './components/verify-email/verify-email';
 import { Home } from './user/home/home';
 import { authGuard } from './shared/guards/auth-guard';
 import { adminGuard } from './shared/guards/admin-guard';
 import { ForgotPassword } from './components/forgot-password/forgot-password';
+import { ResetPassword } from './components/reset-password/reset-password';
 
 export const routes: Routes = [
     {path:'',component:Landing},
@@ -15,6 +16,7 @@ export const routes: Routes = [
     {path:'login',component:Login},
     {path:'verify-email',component:VerifyEmail},
     {path:'forgot-password',component:ForgotPassword},
+    {path:'reset-password',component:ResetPassword},
     {path:'home',component:Home, canActivate:[authGuard]},
     {
         path:'admin',
