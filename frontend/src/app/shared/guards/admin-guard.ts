471ed2ddd86cc344806858e26c91f3c390ee8427
Piyush Kohli <piiussh@gmail.com>
2026-08-19 19:13:50 +0530

    bug fixed

--- frontend/src/app/shared/guards/admin-guard.ts
+++ frontend/src/app/shared/guards/admin-guard.ts
@@ -2,9 +2,11 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
-export const adminGuard: CanActivateFn = (route, state) => {
+export const adminGuard: CanActivateFn = async(route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
+  await authService.initializeAuth();
 
   if(authService.isLoggedIn() && authService.isAdmin()){
     return true;
