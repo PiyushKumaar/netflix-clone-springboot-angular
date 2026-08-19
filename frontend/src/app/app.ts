3482caa9dabe3e25dbd7a55f6389a6beab30ac54
Piyush Kohli <piiussh@gmail.com>
2026-08-19 19:18:37 +0530

    added methods to show header in app.html

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Header } from './shared/commonComp/header/header';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('netflix-clone');
+  showHeader = signal(false);
+
+  constructor(private router: Router) {
+
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+
+        const url = event.urlAfterRedirects;
+
+        this.showHeader.set(
+          url.startsWith('/home') ||
+          url.startsWith('/my-favourites') ||
+          url.startsWith('/admin')
+        );
+        console.log(
+          'SHOW HEADER:',
+          this.showHeader()
+        );
+
+      });
+  }
 }
