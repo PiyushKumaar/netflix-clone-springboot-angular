497045a2f069f81053b32980791cf2a87ed72c35
Piyush Kohli <piiussh@gmail.com>
2026-08-19 19:13:38 +0530

    bug fixed

--- frontend/src/app/shared/guards/auth-guard.ts
+++ frontend/src/app/shared/guards/auth-guard.ts
@@ -2,10 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async(route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  await authService.initializeAuth();
+  
   if(authService.isLoggedIn()){
     return true;
   }
