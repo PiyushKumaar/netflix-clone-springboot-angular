4afe25ed5d3a161c5f335e284bab093ab4915d4c
Piyush Kohli <piiussh@gmail.com>
2026-08-19 19:18:15 +0530

    removed header

--- frontend/src/app/admin/admin-routing-module.ts
+++ frontend/src/app/admin/admin-routing-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Header } from '../shared/commonComp/header/header';
 
 const routes: Routes = [];
 
