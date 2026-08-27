06c4193211326cb807045da1d6deb7fd9f5c931a
Piyush Kohli <piiussh@gmail.com>
2026-08-27 14:46:37 +0530

    added path for users

--- frontend/src/app/admin/admin-routing-module.ts
+++ frontend/src/app/admin/admin-routing-module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VideoList } from './video-list/video-list';
+import { UserList } from './user-list/user-list';
 
 const routes: Routes = [
   {
     path : '',
     children:[
       {path:'',redirectTo:'videos',pathMatch:'full'},
-      {path:'videos', component:VideoList}
+      {path:'videos', component:VideoList},
+      {path:'users',component:UserList}
     ]
   }
 ];
