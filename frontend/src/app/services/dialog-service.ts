c88e4f46953966f8cfde46d1cd330fda9b258ff6
Piyush Kohli <piiussh@gmail.com>
2026-08-27 14:45:34 +0530

    created open dialog for manage user

--- frontend/src/app/services/dialog-service.ts
+++ frontend/src/app/services/dialog-service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { ConfirmDialog } from '../shared/commonComp/confirm-dialog/confirm-dialog';
 import { ManageVideo } from '../admin/dialog/manage-video/manage-video';
 import { VideoPlayer } from '../shared/commonComp/video-player/video-player';
+import { ManageUser } from '../admin/dialog/manage-user/manage-user';
 
 @Injectable({
   providedIn: 'root',
@@ -51,5 +52,12 @@ export class DialogService {
       data:video,
       ...DIALOG_CONFIG.VIDEO_PLAYER
     });
+  }
+
+  openManageUserDialog(mode:'create' | 'edit',user?:any) : MatDialogRef<ManageUser>{
+    return this.dialog.open(ManageUser,{
+      ...DIALOG_CONFIG.MANAGER_USER,
+      data : {mode,user}
+    })
   }
 }
