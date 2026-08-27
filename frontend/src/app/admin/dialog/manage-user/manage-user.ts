3b9372301d227d4cd3233405b2b57e10b840cdd5
Piyush Kohli <piiussh@gmail.com>
2026-08-27 14:46:23 +0530

    created manager-user component

---
+++ frontend/src/app/admin/dialog/manage-user/manage-user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-user',
+  imports: [],
+  templateUrl: './manage-user.html',
+  styleUrl: './manage-user.css',
+})
+export class ManageUser {}
